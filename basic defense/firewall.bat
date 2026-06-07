5467f3170f4baf80e9f721f04ca22b25668b4b31
matteo227 <matteo.perano@icloud.com>
2026-06-07 09:45:17 +0200

    Update firewall.bat

--- basic defense/firewall.bat
+++ basic defense/firewall.bat
@@ -1,11 +1,21 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set WHITELIST_IPS=192.168.1.50
 set BLOCKED_IPS=192.168.1.10 10.0.0.5
-set BLOCKED_PORTS=23 445 3389
+set BLOCKED_PORTS=23 135 139 445 3389
+set ALLOW_PORTS=80 443 22
 
 set IP=%1
 set PORT=%2
+
+set ALLOWED=0
+
+for %%w in (%WHITELIST_IPS%) do (
+    if "%%w"=="%IP%" set ALLOWED=1
+)
+
+if %ALLOWED%==1 exit /b 0
 
 for %%i in (%BLOCKED_IPS%) do (
     if "%%i"=="%IP%" exit /b 1
@@ -15,4 +25,11 @@ for %%p in (%BLOCKED_PORTS%) do (
     if "%%p"=="%PORT%" exit /b 1
 )
 
-exit /b 0
+set PORT_OK=0
+for %%a in (%ALLOW_PORTS%) do (
+    if "%%a"=="%PORT%" set PORT_OK=1
+)
+
+if %PORT_OK%==1 exit /b 0
+
+exit /b 1
